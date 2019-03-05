879673706314e63a0b507457e0f6b6e7f62aabc2
Hristo Dobriyanov <hdobriyanov@gmail.com>
2019-03-05 21:59:56 +0200

    Initial Commit

---
+++ Cars.Data/Models/Engine.cs
@@ -0,0 +1,16 @@
+﻿namespace Cars.Data.Models
+{
+    public class Engine
+    {
+        public int Id { get; set; }
+
+        public string Make { get; set; }
+
+        public double Capacity { get; set; }
+
+        public int Cylinders { get; set; }
+
+        public int HorsePower { get; set; }
+
+    }
+}
