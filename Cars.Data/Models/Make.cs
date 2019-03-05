9ead3a6bf108b14100d5fb0d93ae7ed6aa842870
Hristo Dobriyanov <hdobriyanov@gmail.com>
2019-03-05 22:09:14 +0200

    Updates

---
+++ Cars.Data/Models/Make.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Cars.Data.Models
+{
+    public class Make
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Car> Cars { get; set; }
+
+    }
+}
