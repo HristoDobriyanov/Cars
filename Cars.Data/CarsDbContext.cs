ff7d168c887021e0cb43df0dac4f6be0000cc496
Hristo Dobriyanov <hdobriyanov@gmail.com>
2019-03-06 13:36:07 +0200

    Updates

--- Cars.Data/CarsDbContext.cs
+++ Cars.Data/CarsDbContext.cs
@@ -1,9 +1,35 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Cars.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace Cars.Data
 {
     public class CarsDbContext : DbContext
     {
+        public DbSet<Make> Makes { get; set; }
+
+        public DbSet<Car> Cars { get; set; }
+
+        public DbSet<Engine> Engines { get; set; }
+
+        public DbSet<LicensePlate> LicensePlates { get; set; }
+
+        public DbSet<Dealership> Dealerships { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                builder.UseSqlServer(@"Server=DESKTOP-R4OGD90\SQLEXPRESS;Database=Cars;Integrated Security=True");
+            }
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
