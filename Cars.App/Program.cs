0bbc315f15f27cb42fc8e9e59ea3bca0da209164
Hristo Dobriyanov <hdobriyanov@gmail.com>
2019-03-07 14:27:48 +0200

    Updates

--- Cars.App/Program.cs
+++ Cars.App/Program.cs
@@ -44,13 +44,60 @@ namespace Cars.App
 
             var engines = new[]
             {
-                new Engine{ Capacity = 1.6, Cylinders=4, FuelType = FuelType.Petrol, HorsePower = 95},
+                new Engine
+                {
+                    Capacity = 1.6,
+                    Cylinders =4,
+                    FuelType = FuelType.Petrol,
+                    HorsePower = 95
+                },
+                new Engine
+                {
+                    Capacity = 3.0,
+                    Cylinders =8,
+                    FuelType = FuelType.Diesel,
+                    HorsePower = 318
+                },
+                new Engine
+                {
+                    Capacity = 1.2,
+                    Cylinders =3,
+                    FuelType = FuelType.Gas,
+                    HorsePower = 60
+                }
             };
 
 
             var cars = new [] 
             {
-                new Car{ Make = makes[0] },
+                new Car
+                {
+                    Engine = engines[2],
+                    Make = makes[6],
+                    Doors = 4,
+                    Model = "Кашон-P50",
+                    ProductionYear = new DateTime(1958, 1, 1),
+                    Transmission = Transmission.Manual
+                },
+                new Car
+                {
+                    Engine = engines[1],
+                    Make = makes[5],
+                    Doors = 4,
+                    Model = "Москвич-423",
+                    ProductionYear = new DateTime(1954, 1, 1),
+                    Transmission = Transmission.Automatic
+                },
+                new Car
+                {
+                    Engine = engines[0],
+                    Make = makes[0],
+                    Doors = 4,
+                    Model = "Escort",
+                    ProductionYear = new DateTime(1955, 1, 1),
+                    Transmission = Transmission.Automatic
+                },
+
             };
 
         }
